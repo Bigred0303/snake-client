ef79d853902a8718297e50f141dc39a9620972a0
Grant <grantimaxi03@gmail.com>
2024-04-17 19:31:49 -0400

    Add player messages

--- input.js
+++ input.js
@@ -4,7 +4,11 @@ const keyMap = {
     '\u0077': 'up',
     '\u0061': 'left',
     '\u0073': 'down',
-    '\u0064': 'right'
+    '\u0064': 'right',
+    '\u0068': 'Hello!',
+    '\u0067': 'Goodbye!',
+    '\u0076': 'Victory!'
+
   };
 
 const handleUserInput = function (key) {
@@ -12,9 +16,11 @@ const handleUserInput = function (key) {
         console.log("Exiting Game")
         process.exit();
       }
-      const direction = keyMap[key];
-      if (direction) {
-        connection.write(`Move: ${direction}`);
+      const keyPress = keyMap[key];
+      if (keyPress === 'up' || keyPress === 'left' || keyPress === 'down' || keyPress === 'right') {
+        connection.write(`Move: ${keyPress}`);
+      } else {
+        connection.write(`Say: ${keyPress}`);
       }
   };
 
